147d80e23c7efffd5354596bcbb8632af87f7e6f
SeungUk Ahn <ahn2802@gmail.com>
2023-08-15 05:13:21 +0900

    retrieveTodos 메서드 작성

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -1,11 +1,13 @@
 package com.in28minutes.rest.webservices.restfulwebservices.todo;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class TodoResource {
 
