214ca51e24a1be756b40db5c9ac3305e8b429e2a
SeungUk Ahn <ahn2802@gmail.com>
2023-08-17 03:50:41 +0900

    Spring Security 필터체인 설정: basic auth, stateless session, CSRF 비활성화

---
+++ src/main/java/com/maicoding/rest/webservices/restfulwebservices/basic/BasicAuthenticationSecurityConfiguration.java
@@ -0,0 +1,26 @@
+package com.maicoding.rest.webservices.restfulwebservices.basic;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class BasicAuthenticationSecurityConfiguration {
+    //필터체인
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return
+            http.authorizeHttpRequests(
+                    auth -> auth.anyRequest().authenticated()
+                )
+            .httpBasic(Customizer.withDefaults())  //basic auth - 로그인창
+            .sessionManagement(                     //stateless session 만들기 (CSRF 비활성화 연관)
+                    session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+            .csrf().disable()
+            .build();
+    }
+}
